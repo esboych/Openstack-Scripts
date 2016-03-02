36a6e46082a516560eeacbffc32e0a2562ebd5b7
Yevgen <ysboychakov@mirantis.com>
2016-03-02 18:25:51 +0200

    Rally DB search improved

--- SHAKER_LAUNCH_AUTOMATION/get_Rally_DB_results.sh
+++ SHAKER_LAUNCH_AUTOMATION/get_Rally_DB_results.sh
@@ -8,17 +8,18 @@ FUEL_IP=${FUEL_IP:-172.16.44.10}
 SSH_CMD="sshpass -p ${SSH_PASSWORD} ssh -o ${SSH_OPTIONS} ${SSH_USER}@${FUEL_IP}"
 SCP_CMD="sshpass -p r00tme scp -o ${SSH_OPTIONS} "
 
-#Command tpo connect to Rally DB with runs results
+#Command to connect to Rally DB with runs results
 SQL_CMD="psql postgres://rally:Ra11y@172.18.160.54/rally -c"
 
 #The query itself with all the target jobs_ids listed
 SQL_QUERY=" \"SELECT DISTINCT main.task_name, one.avg as RUN_90_Rl_RHEL, two.avg as RUN_91_Rl_Ubuntu, three.avg as RUN_104_RF_RHEL, four.avg as RUN_105_RF_RHEL,  five.avg as RUN_108_RF_Ubuntu, six.avg as RUN_109_RF_RHEL
 FROM (select task_name from test_timings_summary
-    WHERE (jenkins_job like '11_env_run_rally_light/90/%'
-	OR jenkins_job like '11_env_run_rally_light/91/%'
-	OR jenkins_job like '11_env_run_rally/108/%'
-	OR jenkins_job like '11_env_run_rally/104/%'
-	OR jenkins_job like '11_env_run_rally/105/%'
+	    WHERE (jenkins_job like '11_env_run_rally_light/90/%'
+		OR jenkins_job like '11_env_run_rally_light/91/%'
+		OR jenkins_job like '11_env_run_rally/108/%'
+		OR jenkins_job like '11_env_run_rally/104/%'
+		OR jenkins_job like '11_env_run_rally/105/%'
+		OR jenkins_job like '11_env_run_rally/109/%'
     )) as main
 LEFT OUTER JOIN
     (select avg, jenkins_job, task_name from test_timings_summary where  jenkins_job like '11_env_run_rally_light/90/%') as one on one.task_name = main.task_name
@@ -27,14 +28,12 @@ LEFT OUTER JOIN
 LEFT OUTER JOIN
     (select avg, jenkins_job, task_name from test_timings_summary where  jenkins_job like '11_env_run_rally/104/%') as three on three.task_name = main.task_name
 LEFT OUTER JOIN
-    (select avg, jenkins_job, task_name from test_timings_summary where  jenkins_job = '11_env_run_rally/105/33fcbe0b') as four on four.task_name = main.task_name
+    (select avg, jenkins_job, task_name from test_timings_summary where  jenkins_job like '11_env_run_rally/105/%') as four on four.task_name = main.task_name
 LEFT OUTER JOIN
     (select avg, jenkins_job, task_name from test_timings_summary where  jenkins_job like '11_env_run_rally/108/%') as five on five.task_name = main.task_name
 LEFT OUTER JOIN
-    (select avg, jenkins_job, task_name from test_timings_summary where  jenkins_job like '11_env_run_rally/105/%') as six on six.task_name = main.task_name
+    (select avg, jenkins_job, task_name from test_timings_summary where  jenkins_job like '11_env_run_rally/109/%') as six on six.task_name = main.task_name
 ORDER BY main.task_name;\" "
 
-#SQL_QUERY_SMALL=" \"select task_name from test_timings_summary limit 10;\" "
-
-#$SSH_CMD "cd .. && ls -la"
+#Go to Env11 Master node and run psql query on Rally DB
 $SSH_CMD "$SQL_CMD $SQL_QUERY"
