ccf2812e6be5f97717bb40a36f271e6fe8142817
Yevgen <ysboychakov@mirantis.com>
2016-03-03 11:16:43 +0200

    Update traffic.py from repository. Automate Float IP calc of Controller

--- SHAKER_LAUNCH_AUTOMATION/install_baremetal_agents.sh
+++ SHAKER_LAUNCH_AUTOMATION/install_baremetal_agents.sh
@@ -10,6 +10,16 @@ SSH_OPTS='-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
 SSH_CMD="ssh root@"
 SCP_CMD="scp root@"
 USER_NAME=root
+export CONTROLLER_ADMIN_IP=10.20.0.7
+export MASTER_COMPUTE_ADMIN_IP=10.20.0.5
+export SLAVE_COMPUTE_ADMIN_IP=10.20.0.6
+export SERVER_PORT=18000
+
+export CONTROLLER_PUBLIC_IP=$($SSH_CMD${CONTROLLER_ADMIN_IP} "ifconfig | grep br-ex -A 1 | grep inet | awk ' {print \$2}' | sed 's/addr://g'")
+echo "Controller Public IP: $CONTROLLER_PUBLIC_IP"
+
+export SLAVE_COMPUTE_PRIVATE_IP=$($SSH_CMD${SLAVE_COMPUTE_ADMIN_IP} "ifconfig | grep br-mgmt -A 1 | grep inet | awk ' {print \$2}' | sed 's/addr://g'")
+echo "Slave Compute Private IP: $SLAVE_COMPUTE_PRIVATE_IP"
 
 
 #Define Controller IP adresses
@@ -20,6 +30,7 @@ for i in ${CONTROLLER_IP_ARRAY[@]};
 do
     echo $i
 done
+
 #Define compute IPs
 COMPUTE_IP_ARRAY=($(fuel node | awk '/compute/ {print $10}')) #Array of controller IPs in HA case
 #Print out Controllers IPs
@@ -29,10 +40,14 @@ do
     echo $i
     done
 
-#Get timestamp
-TIME="MSK-$(date +%Y-%m-%d-%H:%M:%S)"
-echo -n "Begin Shaker test(s) at "
-date
+# Update traffic.py file to have stdev and median values in the report
+# get new traffic.py file
+wget -nc https://raw.githubusercontent.com/esboych/Openstack-Scripts/master/SHAKER_LAUNCH_AUTOMATION/traffic.py
+
+#copy it to Controller node
+scp traffic.py root@10.20.0.7:/root/traffic.py
+
+
 
 
 ###################################### Run shaker on Computes ################################################
@@ -40,13 +55,10 @@ date
 #Create script to run on Compute #1
 echo "Run agent on Compute #1"
 
-#NODE_IP=${COMPUTE_IP_ARRAY[0]}
-NODE_IP=10.20.0.6
-
 # Create script for run on compute
-REMOTE_SCRIPT=`${SSH_CMD}$NODE_IP "mktemp"`
+REMOTE_SCRIPT=`${SSH_CMD}$MASTER_COMPUTE_ADMIN_IP "mktemp"`
 
-${SSH_CMD}$NODE_IP "cat > ${REMOTE_SCRIPT}" <<\EOF
+${SSH_CMD}$MASTER_COMPUTE_ADMIN_IP "cat > ${REMOTE_SCRIPT}" <<EOF
 #!/bin/bash -xe
 
 #Fill in sources.list
@@ -68,24 +80,21 @@ iptables -I INPUT -s 172.16.0.0/16 -j ACCEPT
 iptables -I INPUT -s 192.168.0.0/16 -j ACCEPT
 
 #run shaker agent#1
-shaker-agent --agent-id a-001 --server-endpoint 172.16.53.4:18000 --debug
+shaker-agent --agent-id a-001 --server-endpoint $CONTROLLER_PUBLIC_IP:$SERVER_PORT --debug
 EOF
 
 #Run script on remote node and get exit code
-${SSH_CMD}$NODE_IP -f  "bash -xe ${REMOTE_SCRIPT}"
+${SSH_CMD}$MASTER_COMPUTE_ADMIN_IP -f  "bash -xe ${REMOTE_SCRIPT}"
 
 ###################################################
 
 #Create script to run on Compute #2
 echo "Run agent on Compute #2"
 
-#NODE_IP=${COMPUTE_IP_ARRAY[1]}
-NODE_IP=10.20.0.7
-
 # Create script for run on compute
-REMOTE_SCRIPT=`${SSH_CMD}$NODE_IP "mktemp"`
+REMOTE_SCRIPT=`${SSH_CMD}$SLAVE_COMPUTE_ADMIN_IP "mktemp"`
 
-${SSH_CMD}$NODE_IP "cat > ${REMOTE_SCRIPT}" <<\EOF
+${SSH_CMD}$SLAVE_COMPUTE_ADMIN_IP "cat > ${REMOTE_SCRIPT}" <<EOF
 #!/bin/bash -xe
 
 #Fill in sources.list
@@ -110,72 +119,52 @@ iptables -I INPUT -s 192.168.0.0/16 -j ACCEPT
 #iperf -s
 
 #run shaker agent#2
-shaker-agent --agent-id a-002 --server-endpoint 172.16.53.4:18000 --debug
+shaker-agent --agent-id a-002 --server-endpoint $CONTROLLER_PUBLIC_IP:$SERVER_PORT --debug
 EOF
 
 #Run script on remote node and get exit code
-${SSH_CMD}$NODE_IP -f "bash -xe ${REMOTE_SCRIPT}"
+${SSH_CMD}$SLAVE_COMPUTE_ADMIN_IP -f "bash -xe ${REMOTE_SCRIPT}"
 
 
 
 
 ##################################### Run Shaker on Controller ################################################
 
-error
-
-#check SSH
-#NODE_IP=${CONTROLLER_IP_ARRAY[0]}
-#NODE_IP=172.16.53.68
-NODE_IP=10.20.0.4
 echo "Run Shaker on Controller"
-echo "Controller IP: $NODE_IP"
-
-# Copy keys to Controller node
-##${SCP_CMD}$NODE_IP:/root/.ssh/id_rsa id_rsa.${USER_NAME}
-
-# Create remote dir.
-#REPORTS_DIR=`${SSH_CMD}$NODE_IP "mktemp -d"`
-#echo "Created reports dir name: " $REPORTS_DIR
-
 
 # Create script for run on controller
-REMOTE_SCRIPT=`${SSH_CMD}$NODE_IP "mktemp"`
+REMOTE_SCRIPT=`${SSH_CMD}$CONTROLLER_ADMIN_IP "mktemp"`
 
 echo "!!! Running Controller"
-${SSH_CMD}$NODE_IP "cat > ${REMOTE_SCRIPT}" <<\EOF
+${SSH_CMD}$CONTROLLER_ADMIN_IP "cat > ${REMOTE_SCRIPT}" <<EOF
 #!/bin/bash -xe
 
 #kill shaker agent if already run
 killall shaker || true
 
-SHAKER_PATH=${SHAKER_PATH}
-TEST_SUBJECT=${TEST_SUBJECT:-networking}
-REPORTS_DIR=`mktemp -d`
-SERVER_ENDPOINT=172.16.53.4
-# get br-ex IP address to use with --server-endpoint Shaker's option
-#SERVER_ENDPOINT=`ifconfig | grep "br-ex" -A 3 | grep "inet addr" | awk '{print $2}' | sed 's/addr://g'`
-SERVER_PORT=18000
-
 # Prepare environment
 source /root/openrc
 
-#apt-get -y install python-dev libzmq-dev python-pip && pip install pbr pyshaker
+apt-get -y install python-dev libzmq-dev python-pip && pip install pbr pyshaker
+
+#copy traffic.py to destination
+cp traffic.py /usr/local/lib/python2.7/dist-packages/shaker/engine/aggregators
+
+#edit v2.py file in order to use keystone v2 api
+sed -i "s/'\/tokens/'\/v2.0\/tokens/" /usr/lib/python2.7/dist-packages/keystoneclient/auth/identity/v2.py
 
 iptables -I INPUT -s 10.20.0.0/16 -j ACCEPT
 iptables -I INPUT -s 10.0.0.0/16 -j ACCEPT
 iptables -I INPUT -s 172.16.0.0/16 -j ACCEPT
 iptables -I INPUT -s 192.168.0.0/16 -j ACCEPT
 
-#shaker-image-builder --debug
-
 #Creating a scenario file
-printf 'description:\n   This scenario run bare agents \n\ndeployment:\n  agents:\n  -\n   id: a-001\n   ip: 10.20.1.2\n   mode: master\n   slave_id: a-002\n\n  -\n   id: a-002\n   ip: 192.168.0.6\n   mode: slave\n   master_id: a-001\n\nexecution:\n  tests:\n  -\n    title: Iperf TCP\n    class: iperf_graph\n    threads: 22\n    time: 180\n' \
+printf 'description:\n   This scenario run bare agents \n\ndeployment:\n  agents:\n  -\n   id: a-001\n   ip: 10.20.1.2\n   mode: master\n   slave_id: a-002\n\n  -\n   id: a-002\n   ip: $SLAVE_COMPUTE_PRIVATE_IP\n   mode: slave\n   master_id: a-001\n\nexecution:\n  tests:\n  -\n    title: Iperf TCP\n    class: iperf_graph\n    threads: 22\n    time: 180\n' \
 > /usr/local/lib/python2.7/dist-packages/shaker/scenarios/networking/static_agents_pair_baremetal.yaml
 
-echo "SERVER_ENDPOINT: $SERVER_ENDPOINT"
-shaker --server-endpoint $SERVER_ENDPOINT:18000 --scenario networking/static_agents_pair_baremetal --report perf_baremetal_nodes_VLAN.html --debug
+shaker --server-endpoint $CONTROLLER_PUBLIC_IP:$SERVER_PORT --scenario networking/static_agents_pair_baremetal --report perf_baremetal_nodes_VLAN.html --debug
 
 EOF
 
 #Run script on remote node and get exit code
-${SSH_CMD}$NODE_IP "bash -xe ${REMOTE_SCRIPT}"
+${SSH_CMD}$CONTROLLER_ADMIN_IP "bash -xe ${REMOTE_SCRIPT}"
